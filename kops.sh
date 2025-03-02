53774936207b091855f608a7d78a472ff354115e
rebel <123311648+Prashanthpanjaala@users.noreply.github.com>
2025-03-02 11:58:37 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -11,8 +11,8 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket prashanth18.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket prashanth18.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://prashanth18.k8s.local
-kops create cluster --name prashanth18.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
-kops update cluster --name prashanth18.k8s.local --yes --admin
+aws s3api create-bucket --bucket panjaala.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket panjaala.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://panjaala.k8s.local
+kops create cluster --name panjaala.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops update cluster --name panjaala.k8s.local --yes --admin
